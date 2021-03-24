feb99fba8ab71c46d221bc242fc7065db579bf28
João Lourençoo <jrelourenco@outlook.pt>
2021-03-24 17:42:40 +0000

    feat(B): Create project

---
+++ projects/lib-publish-version/b/src/lib/b.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BComponent } from './b.component';
+
+
+
+@NgModule({
+  declarations: [BComponent],
+  imports: [
+  ],
+  exports: [BComponent]
+})
+export class BModule { }
